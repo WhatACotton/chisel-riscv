1cbc4a572854352e4650934ce4b658f0a18164ec
WhatACotton <whatacotton@gmail.com>
2024-06-06 04:41:40 +0900

    2-16

--- src/main/scala/05/Core.scala
+++ src/main/scala/05/Core.scala
@@ -17,10 +17,13 @@ class Core extends Module {
   val br_flg = Wire(Bool())
   val br_target = Wire(UInt(WORD_LEN.W))
 
+  val jmp_flg = (inst === JAL || inst === JALR)
+  val alu_out = Wire(UInt(WORD_LEN.W))
   val pc_next = MuxCase(
     pc_plus4,
     Seq(
-      br_flg -> br_target
+      br_flg -> br_target,
+      jmp_flg -> alu_out
     )
   )
 
@@ -38,6 +41,9 @@ class Core extends Module {
 
   val imm_b = Cat(inst(31), inst(7), inst(30, 25), inst(11, 8))
   val imm_b_sext = Cat(Fill(19, imm_b(11)), imm_b, 0.U(1.U))
+
+  val imm_j = Cat(inst(31), inst(19, 12), inst(20), inst(30, 21))
+  val imm_j_sext = Car(Fill(11, imm_j(19)), imm_j, 0.U(1.U))
 
   val rs1_data =
     Mux((rs1_addr =/= 0.U(WORD_LEN.W)), regfile(rs1_addr), 0.U(WORD_LEN.W))
@@ -74,7 +80,9 @@ class Core extends Module {
       BGE -> List(BR_BLT, OP1_RS1, OP2_RS2, MEN_X, REN_X, WB_ALU),
       BGEU -> List(BR_BGE, OP1_RS1, OP2_RS2, MEN_X, REN_X, WB_ALU),
       BLT -> List(BR_BLTU, OP1_RS1, OP2_RS2, MEN_X, REN_X, WB_ALU),
-      BLTU -> List(BR_BGEU, OP1_RS1, OP2_RS2, MEN_X, REN_X, WB_ALU)
+      BLTU -> List(BR_BGEU, OP1_RS1, OP2_RS2, MEN_X, REN_X, WB_ALU),
+      JAL -> List(ALU_ADD, OP1_PC, OP2_IMJ, MEN_X, REN_S, WB_PC),
+      JALR -> List(ALU_JALR, OP1_RS1, OP2_IMI, MEN_X, REN_S, WB_PC)
     )
   )
   val exe_fun :: op1_sel :: op2_sel :: mem_wen :: wb_sel :: Nil = csignals
@@ -82,7 +90,8 @@ class Core extends Module {
   val op1_data = MuxCase(
     0.U(WORD_LEN.W),
     Seq(
-      (op1_sel === OP1_RS1) -> rs1_data
+      (op1_sel === OP1_RS1) -> rs1_data,
+      (op1_sel === OP1_PC) -> pc_reg
     )
   )
 
@@ -91,11 +100,12 @@ class Core extends Module {
     Seq(
       (op2_sel === OP2_RS2) -> rs2_data,
       (op2_sel === OP2_IMI) -> imm_i_sext,
-      (op2_sel === OP2_IMS) -> imm_s_sext
+      (op2_sel === OP2_IMS) -> imm_s_sext,
+      (op2_sel === OP2_IMJ) -> imm_j_sext
     )
   )
 
-  val alu_out = MuxCase(
+  alu_out := MuxCase(
     0.U(WORD_LEN.W),
     Seq(
       (exe_fun === ALU_ADD) -> (op1_data + op2_data),
@@ -107,7 +117,8 @@ class Core extends Module {
       (exe_fun === ALU_SRL) -> (op1_data >> op2_data(4, 0)).asUInt(),
       (exe_fun === ALU_SRA) -> (op1_data.asSInt() >> op2_data(4, 0)).asUInt(),
       (exe_fun === ALU_SLT) -> (op1_data.asSInt() < op2_data.asSInt()).asUInt(),
-      (exe_fun === ALU_SLTU) -> (op1_data < op2_data).asUInt()
+      (exe_fun === ALU_SLTU) -> (op1_data < op2_data).asUInt(),
+      (exe_fun === ALU_JALR) -> (op1_data + op2_data) & ~1.U(WORD_LEN.W)
     )
   )
 
@@ -131,7 +142,10 @@ class Core extends Module {
 
   val wb_data = MuxCase(
     alu_out,
-    Seq((wb_sel === WB_MEM) -> io.dmem.rdata)
+    Seq(
+      (wb_sel === WB_MEM) -> io.dmem.rdata,
+      (wb_sel === WB_PC) -> pc_plus4
+    )
   )
 
   when(rf_wen === REN_S) {

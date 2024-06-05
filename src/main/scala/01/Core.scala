9945d50a9cff82f251ee461e16a1ad74afcc65df
WhatACotton <whatacotton@gmail.com>
2024-06-06 00:46:37 +0900

    directory refact & 2-7

--- src/main/scala/01/Core.scala
+++ src/main/scala/01/Core.scala
@@ -12,12 +12,12 @@ class Core extends Module {
   val regfile = Mem(32, UInt(WORD_LEN.W))
   val pc_reg = RegInit(START_ADDR)
   pc_reg := pc_reg + 4.U(WORD_LEN.W)
-
   io.imem.addr := pc_reg
   val inst = io.imem.inst
-  io.exit := (inst === 0x34333231.U(WORD_LEN.W))
 
   printf(p"pc_reg : 0x${Hexadecimal(pc_reg)}\n")
-  printf(p"inst : 0x${Hexadecimal(inst)}\n")
-  printf("------------------\n")
+  printf(p"inst   : 0x${Hexadecimal(inst)}\n")
+  printf("---------\n")
+  io.exit := (inst === 0x34333231.U(WORD_LEN.W))
+
 }
